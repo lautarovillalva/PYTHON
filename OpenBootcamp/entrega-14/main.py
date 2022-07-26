c131bf46f49bffa9ff56b9533dd5b137b508fc1c
Lautaro Villalva <ljvillalva@gmail.com>
2022-07-26 13:50:05 -0300

    .

---
+++ OpenBootcamp/entrega-14/main.py
@@ -0,0 +1,10 @@
+from operaciones import operaciones as op
+
+def main():
+    print('suma',op.sumar(12,5))
+    print('resta',op.restar(12,5))
+    print('division',op.dividir(12,5))
+    print('multiplicacion',op.multiplicar(12,5))
+
+if __name__=='__main__':
+    main()
---
+++ OpenBootcamp/paquetes_y_modulos/main.py
@@ -0,0 +1,13 @@
+#from operadores import operaciones as op1, operaciones2 as op2
+from operadores import * 
+
+def main():
+    #print(op2.multiplicar(3,9))
+    #print(op1.sumar(18,3))
+    
+    print(operaciones2.multiplicar(3,9))
+    print(operaciones.sumar(18,3))
+
+
+if __name__=='__main__':
+    main()
